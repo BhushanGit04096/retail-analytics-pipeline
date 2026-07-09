1d6dabcd94ecf9661ce08b983f986ecd5d18b6a2
BhushanGit04096 <nagabhushancherry@gmail.com>
2026-07-09 14:46:47 +0530

    Delete src/utils directory

--- src/utils/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open("config/project_config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-
-PROJECT_ID = config["project_id"]
-REGION = config["region"]
-BUCKET_NAME = config["bucket_raw"]
-DATASET_NAME = config["dataset_warehouse"]
